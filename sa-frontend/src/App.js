6707a1547bccbcadfb498b88f3682abffc6d1d69
Kathy <pohsingc@andrew.cmu.edu>
2023-10-05 23:45:08 -0400

    Code changes

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://34.23.82.200:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
